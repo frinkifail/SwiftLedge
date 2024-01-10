71474b90718d012e64e44813ed6e173be6aa6770
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2024-01-10 15:15:39 +0700

    wallclimb (real)

---
+++ src/starterchar/initialize.client.ts
@@ -0,0 +1,27 @@
+import { wallclimb } from './wallclimb';
+const plr = game.GetService('Players').GetPlayerFromCharacter(script.Parent!.Parent)!;
+const hum = <Humanoid>script.Parent!.Parent!.FindFirstChild('Humanoid')!;
+const cam = game.Workspace.CurrentCamera!;
+const hrp = <Part>script.Parent!.Parent!.FindFirstChild('HumanoidRootPart')!;
+// const scs = game.GetService("StarterPlayer").FindFirstChild('StarterCharacterScripts')!
+// interface Modules {
+//     wallclimb: ModuleCallback
+// }
+// const modules = <Modules>{
+//     wallclimb: require(<ModuleScript>scs.FindFirstChild('TS')!.FindFirstChild('wallclimb')!)
+// }
+
+plr.CameraMode = Enum.CameraMode.LockFirstPerson;
+
+game.GetService("UserInputService").InputBegan.Connect((input, gpe) => {
+    if (gpe) return;
+    switch (input.KeyCode) {
+        case Enum.KeyCode.Space:
+            wallclimb(cam, hrp);
+            break;
+        case Enum.KeyCode.LeftShift:
+            // TODO: implement sliding
+            break;
+    }
+})
+
---
+++ src/starterchar/wallclimb.ts
@@ -0,0 +1,14 @@
+export function wallclimb(cam: Camera, hrp: Part) {
+    const lookvector = cam.CFrame.LookVector
+    const raycast = game.Workspace.Raycast(cam.CFrame.Position.add(lookvector), lookvector)!
+    const inst = raycast.Instance
+    const notAllowed = new Set([
+        "Handle",
+        "Head",
+        hrp.Name,
+        "UpperTorso",
+        "LowerTorso"
+    ])
+    if (inst.Size.X > inst.Size.Y || notAllowed.has(inst.Name)) return;
+    hrp.AssemblyLinearVelocity = new Vector3(0, 100);
+}
